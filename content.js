47ae02619f1e7b7d40fb87336104d4f93e289da7
KyuGG <zverolovz@mail.ru>
2022-01-23 21:36:59 +0300

    update v0.2 - added dark theme

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ chrome.runtime.sendMessage({ data: 'settings' }, (response) => {
         removeTrash()
     }
     if (response.answer.checkbox2 == 'true') {
-        //
+        darkTheme()
     }
     if (response.answer.checkbox3 == 'true') {
         //
@@ -32,5 +32,170 @@ function removeTrash() {
         document.querySelectorAll('.bad').forEach(el => el.remove())
         //удаление надписей типа "из 100"
         document.querySelectorAll('.mvb').forEach(el => el.remove())
+    }
+}
+
+function darkTheme() {
+    let black = '#353535'
+    let purple = '#b63dd2'
+    let white = '#fff'
+    document.body.style.backgroundColor = black
+    document.body.style.color = white
+    let path = document.location.pathname
+
+    //темная тема для страницы ЧаВо
+    if (path == '/faq' || path == '/faq/index') {
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('select').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('input').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        //если нажата кнопка искать
+        if (document.querySelectorAll('.newColor').length > 0) {
+            document.querySelectorAll('.panel-heading').forEach((el) => {
+                el.style.backgroundColor = black
+                el.style.color = white
+            })
+
+            document.getElementById('collapse_cat1').style.backgroundColor = black
+            document.querySelectorAll('.newColor').forEach(el => el.style.color = white)
+        }
+    }
+    //темная тема для страницы с баллами
+    if (path == '/student/student') {
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('select').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('ul')[5].style.backgroundColor = black
+    }
+    //темная тема для страницы практика
+    if (path == '/student/practice/index') {
+        document.querySelectorAll('td').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('ul')[5].style.backgroundColor = black
+    }
+    //темная тема для страницы портфолио и страницы отдельной новости
+    if (path.split('/')[1] == 'portfolio' || path == '/main/view-news') {
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('li.active')[1].style.color = purple
+        document.querySelectorAll('td').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('ul')[5].style.backgroundColor = black
+    }
+    //темная тема для страницы учебное портфолио
+    if (path == '/portfolio/list-uchebnie-project' || path == '/portfolio/list-uchebnie-project/index') {
+        document.querySelectorAll('select').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('input').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+    }
+    //темная тема для страницы заявки
+    if (path.split('/')[1] == 'request') {
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('li.active')[2].style.color = purple
+        document.querySelectorAll('select').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('ul')[5].style.backgroundColor = black
+    }
+    //темная тема для страницы домашние задания
+    if (path == '/student/homework/list') {
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('select').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('input').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+    }
+    //темная тема для конкретного ДЗ
+    if (path == '/student/homework/view') {
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('li.active')[1].style.color = purple
+
+        let textarea = document.getElementById('comment-text')
+        textarea.style.backgroundColor = black
+        textarea.style.color = white
+
+        let dragnDrop = document.querySelector('.resumable-drop')
+        dragnDrop.style.backgroundColor = black
+        dragnDrop.style.color = white
+
+        document.querySelectorAll('.author').forEach(el => el.style.color = white)
+        document.querySelectorAll('.list-group-item').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+    }
+    //темная тема для страницы проекта из вкладки портфолио
+    if (path == '/portfolio/view-project') {
+        document.querySelectorAll('input').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('select').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('li.active')[2].style.color = '#b63dd2'
+
+        let textarea = document.getElementById('add_comment')
+        textarea.style.backgroundColor = black
+        textarea.style.color = white
+    }
+    //темная тема для страницы проектная работа
+    if (path == '/social/project_work/project-list' || path == '/social/project_work/project-list/index') {
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('select').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('input').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('ul')[5].style.backgroundColor = black
+    }
+    //темная тема для страницы помощь
+    if (path == '/support/list') {
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+        document.querySelectorAll('select').forEach((el) => {
+            el.style.backgroundColor = black
+            el.style.color = white
+        })
+        document.querySelectorAll('ul')[5].style.backgroundColor = black
+    }
+    //темная тема для страницы электронные библиотеки
+    if (path == '/other/libraries') {
+        document.querySelectorAll('ul')[5].style.backgroundColor = black
+    }
+    //темная тема для страницы профиль
+    if (path == '/user/profile') {
+        document.querySelector('.panel').style.backgroundColor = black
+        document.querySelector('.panel-footer').style.backgroundColor = black
+    }
+    //темная тема для страницы личные файлы
+    if (path == '/personal/files' || path == '/personal/files/index') {
+        document.querySelectorAll('ul')[5].style.backgroundColor = black
+        document.querySelectorAll('.well').forEach(el => el.style.backgroundColor = black)
+
+        let li = document.querySelectorAll('li.active')
+        li[2].style.color = purple
+        li[3].style.color = purple
+
+        let dragnDrop = document.querySelector('.resumable-drop')
+        dragnDrop.style.backgroundColor = black
+        dragnDrop.style.color = white
     }
 }
