33ed854c410af5bafc70a7261c3901122228f3b0
KyuGG <zverolovz@mail.ru>
2022-05-07 07:29:46 +0300

    logo favicon

--- functions/logo.js
+++ functions/logo.js
@@ -1,4 +1,10 @@
 function logo() {
+    const icon = document.createElement('link')
+    icon.rel = 'icon'
+    icon.type = 'image/png'
+    icon.href = chrome.runtime.getURL('interface/logo.png')
+    document.querySelector('head').append(icon)
+
     if (location.pathname == '/user/profile') {
         const logo = document.querySelector('.img-responsive')
         logo.src = chrome.runtime.getURL('interface/logo.png')
