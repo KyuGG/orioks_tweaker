77d95337fabd3cb868654cf544c6132a5dbc2d6f
KyuGG <zverolovz@mail.ru>
2022-11-30 15:38:32 +0300

    styled popup

--- src/entry/content.ts
+++ src/entry/content.ts
@@ -1,5 +1,26 @@
 import renderTest from './renderTest'
+import StorageSettings from '@/interfaces/StorageSettings'
 
 if (location.href === 'https://orioks.miet.ru/test') {
     renderTest()
 }
+
+if (location.href === 'https://orioks.miet.ru/student/practice/index') {
+    console.log('object')
+}
+
+// chrome.runtime.sendMessage(
+//     {
+//         data: 'changeSettings',
+//         settings: {
+//             fixScore: true,
+//             fixDownload: false,
+//             schedule: false,
+//             disciplineNames: false,
+//             darkTheme: false,
+//         } as StorageSettings['settings'],
+//     },
+//     response => {
+//         console.log(response)
+//     }
+// )
--- src/view/popup/popup.vue
+++ src/view/popup/popup.vue
@@ -1,53 +1,134 @@
 <template>
-    <div>
+    <div class="popup"
+         v-if="(settings && version)">
         <h2>ORIOKS tweaker {{ version }}</h2>
         <div class="switcher">
-            <input type="checkbox" class="ios8-switch" id="checkbox-all" />
+            <Switcher label="All Settings"
+                      id="allSettings"
+                      :checked="(settings.fixScore && settings.fixDownload && settings.schedule && settings.disciplineNames && settings.darkTheme)"
+                      @update="(checked: boolean) => settings.fixScore = settings.fixDownload = settings.schedule = settings.disciplineNames = settings.darkTheme = !checked">
+            </Switcher>
+        </div>
+        <h3>Basic Settings</h3>
+        <div class="switcher">
+            <Switcher label="Fix Score"
+                      id="fixScore"
+                      :checked="settings.fixScore"
+                      @update="(checked: boolean) => settings.fixScore = !checked">
+            </Switcher>
+        </div>
+        <div class="switcher">
+            <Switcher label="Fix Download"
+                      id="fixDownload"
+                      :checked="settings.fixDownload"
+                      @update="(checked: boolean) => settings.fixDownload = !checked">
+            </Switcher>
+        </div>
+        <div class="switcher">
+            <Switcher label="Schedule"
+                      id="schedule"
+                      :checked="settings.schedule"
+                      @update="(checked: boolean) => settings.schedule = !checked">
+            </Switcher>
+        </div>
+        <h3>Customize</h3>
+        <div class="switcher">
+            <Switcher label="Discipline Names"
+                      id="disciplineNames"
+                      :checked="settings.disciplineNames"
+                      @update="(checked: boolean) => settings.disciplineNames = !checked">
+            </Switcher>
+        </div>
+        <div class="switcher">
+            <Switcher label="Dark Theme"
+                      id="darkTheme"
+                      :checked="settings.darkTheme"
+                      @update="(checked: boolean) => settings.darkTheme = !checked">
+            </Switcher>
+        </div>
+        <div class="buttons">
+            <input id="bugreport"
+                   type="button"
+                   value="Report a Bug"
+                   @click="bugReport">
+            <input id="description"
+                   type="button"
+                   value="Description"
+                   @click="description">
+        </div>
+
+        <!-- {{ settings.fixDownload }} -->
+        <!-- <div class="switcher">
+            <input type="checkbox"
+                   class="ios8-switch"
+                   id="checkbox-all" />
             <label for="checkbox-all">All Settings</label>
         </div>
         <h3>Basic Settings</h3>
         <div class="switcher">
-            <input type="checkbox" class="ios8-switch" id="checkbox-1" />
+            <input type="checkbox"
+                   class="ios8-switch"
+                   id="checkbox-1" />
             <label for="checkbox-1">Fix Score</label>
         </div>
         <br />
         <div class="switcher">
-            <input type="checkbox" class="ios8-switch" id="checkbox-2" />
+            <input type="checkbox"
+                   class="ios8-switch"
+                   id="checkbox-2" />
             <label for="checkbox-2">Fix Download</label>
         </div>
         <br />
         <div class="switcher">
-            <input type="checkbox" class="ios8-switch" id="checkbox-3" />
+            <input type="checkbox"
+                   class="ios8-switch"
+                   id="checkbox-3" />
             <label for="checkbox-3">Schedule</label>
         </div>
         <h3>Customize</h3>
         <div class="switcher">
-            <input type="checkbox" class="ios8-switch" id="checkbox-4" />
+            <input type="checkbox"
+                   class="ios8-switch"
+                   id="checkbox-4" />
             <label for="checkbox-4">Discipline Names</label>
         </div>
         <br />
         <div class="switcher">
-            <input type="checkbox" class="ios8-switch" id="checkbox-5" />
+            <input type="checkbox"
+                   class="ios8-switch"
+                   id="checkbox-5" />
             <label for="checkbox-5">Dark Theme</label>
         </div>
         <div>
-            <input id="bugreport" type="button" value="Report a Bug" />
-            <input id="description" type="button" value="Description" />
-        </div>
+            <input id="bugreport"
+                   type="button"
+                   value="Report a Bug" />
+            <input id="description"
+                   type="button"
+                   value="Description" />
+        </div> -->
     </div>
 </template>
 
 <script setup lang="ts">
-const version = ''
+import StorageSettings from '@/interfaces/StorageSettings'
+import { ref } from 'vue'
+import Switcher from './switcher.vue'
 
-const checkboxAll = document.getElementById('checkbox-all')
-const checkbox1 = document.getElementById('checkbox-1')
-const checkbox2 = document.getElementById('checkbox-2')
-const checkbox3 = document.getElementById('checkbox-3')
-const checkbox4 = document.getElementById('checkbox-4')
-const checkbox5 = document.getElementById('checkbox-5')
-const bugreport = document.getElementById('bugreport')
-const description = document.getElementById('description')
+document.body.style.margin = '0'
+
+const settings = ref(null as unknown as StorageSettings['settings'])
+let version = ''
+
+chrome.runtime.sendMessage({ task: 'getSettings' }, (response: {
+    settings: StorageSettings['settings'],
+    version: StorageSettings['version']
+}) => {
+    settings.value = response.settings
+    version = response.version
+})
+
+
 // window.onload = () => document.body.style.opacity = '100%'
 // document.documentElement.style.setProperty('--setting-transition', 'all .3s')
 // chrome.runtime.sendMessage({ data: 'settingsPopup' }, response => {
@@ -102,12 +183,15 @@ const description = document.getElementById('description')
 //     chrome.runtime.sendMessage({ data: 'changeSettings', settings: settings }, response => console.log(response.answer))
 // }
 
-
 // checkbox1.onclick = onClick
 // checkbox2.onclick = onClick
 // checkbox3.onclick = onClick
 // checkbox4.onclick = onClick
 // checkbox5.onclick = onClick
+
+const bugReport = () => chrome.tabs.create({ url: 'https://orioks.miet.ru/bugreport' })
+const description = () => chrome.tabs.create({ url: 'https://github.com/KyuGG/orioks_tweaker#функционал' })
+
 </script>
 
 <style scoped lang="scss">
@@ -120,25 +204,14 @@ const description = document.getElementById('description')
     user-select: none;
 }
 
-html {
-    width: 100%;
-    height: 100%;
-    background-color: #1d1c1c;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    --setting-transition: none;
+.popup {
+    width: 200px;
+    padding: 30px;
+    background: #1d1c1c
 }
 
-body {
-    width: 250px;
-    padding: 40px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    opacity: 0;
-    transition: 2s;
+.switcher {
+    margin-bottom: 20px;
 }
 
 h2 {
@@ -146,94 +219,26 @@ h2 {
     margin-bottom: 15px;
 }
 
-.switcher {
-    margin-bottom: 10px;
+h2,
+h3 {
+    color: white;
 }
 
-.slider {
-    display: flex;
-    flex-direction: row;
-}
-
-.slider .color-show {
-    width: 30px;
-}
-
-input[type='checkbox'].ios8-switch {
-    position: absolute;
-    margin: 8px 20 0 16px;
-}
-
-input[type='checkbox'].ios8-switch+label {
-    position: relative;
-    padding: 5px 0 0 50px;
-    line-height: 2em;
-}
-
-input[type='checkbox'].ios8-switch+label:before {
-    content: '';
-    position: absolute;
-    display: block;
-    left: 0;
-    top: 0;
-    width: 40px;
-    height: 24px;
-    border-radius: 16px;
-    background: #1d1c1c;
-    border: 1px solid #3b3b3b;
-    -webkit-transition: var(--setting-transition);
-    transition: var(--setting-transition);
-}
-
-input[type='checkbox'].ios8-switch+label:after {
-    content: '';
-    position: absolute;
-    display: block;
-    left: 0px;
-    top: 0px;
-    width: 24px;
-    height: 24px;
-    border-radius: 16px;
-    background: #fff;
-    border: 1px solid #d9d9d9b0;
-    -webkit-transition: var(--setting-transition);
-    transition: var(--setting-transition);
-}
-
-input[type='checkbox'].ios8-switch+label:hover:after {
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-}
-
-input[type='checkbox'].ios8-switch:checked+label:after {
-    margin-left: 16px;
-}
-
-input[type='checkbox'].ios8-switch:checked+label:before {
-    background: #5581d3;
-}
-
-label {
-    transition: var(--setting-transition);
-}
-
-label:hover {
-    color: #5581d3;
-}
-
-#bugreport,
-#description {
-    margin-top: 20px;
+.buttons input {
+    margin-top: 10px;
+    margin-left: 1px;
+    margin-right: 1px;
     padding: 10px;
     border: 1px solid;
     background: #1d1c1c;
     color: #fff;
     transition: 0.3s;
     cursor: pointer;
+
+    &:hover {
+        background: #5581d3;
+        color: black;
+    }
 }
 
-#bugreport:hover,
-#description:hover {
-    background: #5581d3;
-    color: black;
-}
 </style>
