e082d5e20cbb6006633fa5af919b542762d87a97
KyuGG <zverolovz@mail.ru>
2022-12-04 14:55:56 +0300

    getSettings function

---
+++ src/functions/getSettings.ts
@@ -0,0 +1,14 @@
+import GetSettingsResponse from '../interfaces/GetSettingsResponse'
+import wakeUpBackground from './wakeUpBackground'
+
+export default async function getSettings() {
+    await wakeUpBackground()
+
+    const settings: GetSettingsResponse = await new Promise(resolve =>
+        chrome.runtime.sendMessage({ task: 'getSettings' }, response =>
+            resolve(response)
+        )
+    )
+
+    return settings
+}
