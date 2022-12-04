5190ccbb3670051d8f442048eb810fa7774d2ccb
lordmyrnya <vladfokin2002@gmail.com>
2022-12-04 16:09:47 +0300

    changeLogo func

---
+++ src/functions/changeLogo.ts
@@ -0,0 +1,18 @@
+import wakeUpBackground from './wakeUpBackground'
+
+export default async function changeLogo() {
+    await wakeUpBackground()
+    const logoLink = chrome.runtime.getURL('./assets/logo.png')
+    const icon = document.createElement('link')
+    icon.rel = 'icon'
+    icon.type = 'image/png'
+    icon.href = logoLink
+    document.head.append(icon)
+
+    if (location.pathname == '/user/profile') {
+        const logo = document.querySelector(
+            '.img-responsive'
+        ) as HTMLImageElement
+        logo.src = logoLink
+    }
+}
--- src/functions/wakeUpBackground.ts
+++ src/functions/wakeUpBackground.ts
@@ -1,3 +1,4 @@
+/**Функция "пробуждает" background-скрипт. Желательно использовать перед тем, как сделать запрос к хрому. Требует использование await.*/
 export default function wakeUpBackground(): Promise<void> {
     return new Promise(resolve => {
         chrome.runtime.sendMessage({ task: 'wakeUp' }, () => {
