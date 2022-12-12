2a803762ffadb99731aca2774ae6ac72eedab74b
KyuGG <zverolovz@mail.ru>
2022-12-13 00:42:19 +0300

    nav panel component

---
+++ src/components/navPanel.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul class="breadcrumb">
+        <li><a href="/">Главная</a></li>
+        <li><a :href="href">{{ name }}</a></li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    name: string
+}>()
+
+const href = location.href
+</script>
+
+<style scoped lang="scss">
+
+</style>
