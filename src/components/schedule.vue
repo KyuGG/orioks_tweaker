8d2dd8f042d1bb01b4d6980285dd2981cdfde538
KyuGG <zverolovz@mail.ru>
2022-12-13 23:21:39 +0300

    schedule preparations: JSX support (babel plugin)

---
+++ src/components/schedule.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <NavPanel name="Расписание"></NavPanel>
+        <div>SCHEDULE</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import NavPanel from './navPanel.vue'
+
+</script>
