b2eac5147cbba263e9153c200e77d0b6857683bf
KyuGG <zverolovz@mail.ru>
2022-12-15 00:19:08 +0300

    myButton component

---
+++ src/components/myButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button
+        class="my-btn"
+        @click="emit('click')"
+    >{{ placeholder }}</button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    placeholder: string
+}>()
+
+const emit = defineEmits<{
+    (event: 'click'): void
+}>()
+</script>
+
+<style scoped lang="scss">
+.my-btn {
+    width: 160px;
+    margin-right: 2px;
+}
+</style>
