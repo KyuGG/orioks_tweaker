e16561bfb30eb4e03a536157c053ebfc1f49538c
KyuGG <zverolovz@mail.ru>
2022-12-15 00:19:50 +0300

    move schedule components to schedule folder

---
+++ src/components/schedule/schedulePage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <NavPanel name="Расписание"></NavPanel>
+        <h3>Группа: {{ group }}</h3>
+        <MyButton
+            placeholder="Выбрать"
+            @click="choose"
+        ></MyButton>
+        <MyButton
+            placeholder="Подсказки"
+            @click="hints"
+        ></MyButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+import MyButton from '../myButton.vue'
+import NavPanel from '../navPanel.vue'
+
+const group = 'GROUP'
+
+const choose = () => console.log('choose')
+const hints = () => console.log('hints')
+</script>
+
+<style scoped lang="scss">
+h3 {
+    margin-top: 10px;
+    border: 1px solid #dddddd;
+    padding: 5px;
+}
+</style>
