57d7937b68777081400b1d6823e5928e75869b11
lordmyrnya <vladfokin2002@gmail.com>
2022-12-19 00:47:12 +0300

    hotfix

--- content.js
+++ content.js
@@ -2,25 +2,28 @@ document.documentElement.style.visibility = 'hidden'
 
 getSettings().then(settings => {
     changeTheme()
-
-    window.onload = async () => {
-        logo()
-        bugReport()
-        await sleep(100)
-        if (settings.answer.checkbox1) fixScore()
-        if (settings.answer.checkbox2) runDownload()
-        if (settings.answer.checkbox3) await schedule()
-        if (settings.answer.checkbox4) discNameChanger()
-        if (settings.answer.checkbox5) coloredElements()
-        discNameLoader()
-        document.documentElement.style.visibility = 'visible'
-        await sleep(500)
-        document.documentElement.style.setProperty('--transition', '.3s')
-    }
 })
 
+window.addEventListener('load', onloadFunc)
 
-function getSettings() {
+async function onloadFunc(){
+    const settings = await getSettings()
+    logo()
+    bugReport()
+    await sleep(100)
+    if (settings.answer.checkbox1) fixScore()
+    if (settings.answer.checkbox2) runDownload()
+    if (settings.answer.checkbox3) await schedule()
+    if (settings.answer.checkbox4) discNameChanger()
+    if (settings.answer.checkbox5) coloredElements()
+    discNameLoader()
+    document.documentElement.style.visibility = 'visible'
+    await sleep(500)
+    document.documentElement.style.setProperty('--transition', '.3s')
+}
+
+
+async function getSettings() {
     return new Promise(resolve => {
         chrome.runtime.sendMessage({ data: 'settings' }, response => {
             if (!chrome.runtime.lastError)
