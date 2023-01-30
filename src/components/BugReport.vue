df6b11159ea437b393f10847dc97eb752cd21801
KyuGG <zverolovz@mail.ru>
2023-01-30 08:39:01 +0300

    финт ушами (2 часть)

--- src/components/BugReport.vue
+++ src/components/BugReport.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import NavPanel from './NavPanel1.vue'
+import NavPanel from './NavPanel.vue'
 
 </script>
 
--- src/functions/bugReport.ts
+++ src/functions/bugReport.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from '@/components/BugReport1.vue'
+import App from '@/components/BugReport.vue'
 
 /** Встраивает vue компонент на отдельную страницу сайта с возможностью багрепорта */
 export default function bugReport() {
